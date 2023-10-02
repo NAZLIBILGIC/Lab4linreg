8862e4af7976b5a523d60dbc08956b7c0605865f
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-10-02 13:22:53 +0200

    Roxygen documentation added

--- R/linreg.R
+++ R/linreg.R
@@ -1,5 +1,36 @@
 library(ggplot2)
-
+#'A function for the multiple regression model
+#'@description
+#'The function receives 2 arguments formula and data and uses linear algebra and returns of class linreg as an RC class.
+#'A reference class called "linreg" is defined using "setRefClass". It is used to perform a linear regression analysis on a data set providing the summary of the results.The class and the methods are designed to make the linear regression model more user friendly and organised.
+#'
+#'The fields list is created to define the fields that an object of the linreg class will have.
+#'
+#'The methods list is created to define the methods (functions) that can be applied to objects of the linreg class.These methods assist to perform various operations on the linear regression model.
+#'
+#'Initialize: Constructor method for creating a linreg object. It takes a formula and data as arguments, stores them, and then calls fit_model to perform the regression.
+#'The fit model uses the provided formula and data, calculating coefficients, residuals, and various statistics.
+#'The resid returns the residuals as a vector.
+#'The pred returns the predicted values.
+#'The coef returns the coefficients with their names.
+#'The summary prints a summary of the linear regression model, including coefficients, standard errors, t-values, p-values, residual standard error, and variance of coefficients.
+#'The show displays basic information about the linreg object, including the formula, data, and coefficients.
+#'The plot generates and displays two diagnostic plots for the linear regression model: "Residuals vs Fitted" and "Scale-Location".
+#'
+#'@param formula The formula argument should take a formula object. It includes the formula given from the user so that the function will do the calculations for the regression.
+#'@param data It must be a data frame and it includes the data for the regression. 
+#'@param coefficients It must be a matrix. A matrix to store the regression coefficients.
+#'@param residuals It must be an array. It is used to store the residuals.
+#'@param predicted It must be a matrix. It is used to store the predicted values.
+#'@param degrees_of_freedom The type should be numeric. It is used to store the degrees of freedom.
+#'@param residual_variance A numeric value for the residual variance.
+#'@param variance_of_coefficients It must be a matrix. It is used to store the variance of the coefficients.
+#'@param t_values It must be a matrix to store the t values.
+#'@param p_values It must be a matrix to store the p values.
+#'
+#'@return The function returns an object of class linreg as an RC class.
+#'
+#'@export
 # Define the linreg class
 linreg <- setRefClass(
   "linreg",
@@ -26,22 +57,22 @@ linreg <- setRefClass(
       y <- .self$data[[all.vars(.self$formula)[1]]]
       n <- nrow(X)
       p <- ncol(X)
-
+      
       beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
       y_hat <- X %*% beta_hat
       residuals <- y - y_hat
       df <- n - p
       sigma_sq_hat <- sum(residuals^2) / df
       Var_beta_hat <- sigma_sq_hat * solve(t(X) %*% X)
-
+      
       # Calculate t-values and p-values
-      t_values <- beta_hat / sqrt(diag(Var_beta_hat))
-      p_values <- 2 * (1 - pt(abs(t_values), df))
-
+      t_values <<- beta_hat / sqrt(diag(Var_beta_hat))
+      p_values <<- 2 * (1 - pt(abs(t_values), df))
+      
       # Store the computed statistics
       .self$coefficients <- beta_hat
       .self$residuals <- residuals
-      .self$predicted <- y_hat
+      .self$predicted <<- y_hat
       .self$degrees_of_freedom <- df
       .self$residual_variance <- sigma_sq_hat
       .self$variance_of_coefficients <- Var_beta_hat
@@ -62,33 +93,41 @@ linreg <- setRefClass(
     },
     summary = function() {
       cat("Regression Summary:\n")
-
-      cat("(Intercept) Sepal.Width  Sepal.Length\n")
-      cat(sprintf("  % 9.2f % 9.2f % 9.2f ***\n",
+      
+      cat("Coefficients:\n")
+      cat("             Estimate  Std. Error  t value  Pr(>|t|)\n")
+      cat(sprintf("(Intercept) %9.4f %9.4f %7.2f %9.2e ***\n",
                   .self$coefficients[1],
-                  .self$coefficients[2],
-                  .self$coefficients[3]))
-
+                  sqrt(.self$variance_of_coefficients[1, 1]),
+                  .self$t_values[1],
+                  .self$p_values[1]))
+      for (i in 2:length(.self$coefficients)) {
+        cat(sprintf("Species%s  %9.4f %9.4f %7.2f %9.2e \n",
+                    colnames(.self$coefficients)[i],
+                    .self$coefficients[i],
+                    sqrt(.self$variance_of_coefficients[i, i]),
+                    .self$t_values[i],
+                    .self$p_values[i]))
+      }
+      
       cat("Residual standard error: ")
-      cat(sprintf("%.2f", sqrt(.self$residual_variance)))
-      cat(" on ")
-      cat(.self$degrees_of_freedom)
-      cat(" degrees of freedom\n")
-
+      cat(sprintf("%.2f on %d degrees of freedom\n",
+                  sqrt(.self$residual_variance), .self$degrees_of_freedom))
+      
       cat("Residual Variance: ")
-      cat(sprintf("%.7f", .self$residual_variance))
-      cat("\n")
-
+      cat(sprintf("%.7f\n", .self$residual_variance))
+      
       cat("Variance of Coefficients:\n")
       print(.self$variance_of_coefficients)
-
+      
       cat("\nT-values:\n")
-      print(.self$t_values)
-
+      print (.self$t_values)
+      
       cat("\nP-values:\n")
       print(.self$p_values)
     },
-
+    
+    
     show = function() {
       cat("linreg")
       cat("\nFormula: ", as.character(.self$formula))
@@ -96,10 +135,11 @@ linreg <- setRefClass(
       cat("\nCoefficients:\n")
       print(.self$coef())
     },
-
+    
+    
     plot = function() {
       df <- data.frame(Residuals = .self$resid(), Fitted = .self$pred())
-
+      
       p1 <- ggplot(df, aes(x = Fitted, y = Residuals)) +
         geom_point() +
         stat_summary(
@@ -111,10 +151,10 @@ linreg <- setRefClass(
         labs(title = "Residuals vs Fitted", x = "Fitted values", y = "Residuals") +
         theme(axis.title.y = element_text(vjust = 0.5, size = 10)
         )
-
-      std_residuals <- .self$residuals / sd(.self$residuals)
+      
+      std_residuals <<- .self$residuals / sd(.self$residuals)
       sqrt_std_residuals <- sqrt(abs(std_residuals))
-
+      
       df_std <- data.frame(Fitted = .self$pred(), Sqrt_Std_Residuals = sqrt_std_residuals)
       p2 <- ggplot(df_std, aes(x = Fitted, y = Sqrt_Std_Residuals)) +
         geom_point() +
@@ -131,18 +171,22 @@ linreg <- setRefClass(
           plot.title = element_text(size = 10, hjust = 0.5),
           theme_bw()
         )
-
+      
       # Print the plots
       print(p1)
       print(p2)
     }
-      )
   )
+)
 
-  data(iris)
-  mod_object <- linreg(Petal.Length~Species, data = iris)
 
-  # Use the "show" method to display the object
-  mod_object$summary()
+
+
+data(iris)
+mod_object <- linreg(Petal.Length~Species, data = iris)
+
+mod_object$summary()
+
+
 
 
